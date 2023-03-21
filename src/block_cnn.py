out_classes in model, added trainer for ease of use, divided dataset on train and val, added val loss calculation, added strides to block_cnn.py, fixed some buggs in block_cnn.py

src/block_cnn.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch
 
 
-def get_block_cnn(in_dims, channels, fc_size=256):
+def get_block_cnn(in_dims, channels, strides, fc_size=256):
     layers = []
     kernel_size = 3
     for i in range(len(channels) - 1):
@@ -11,7 +11,8 @@ def get_block_cnn(in_dims, channels, fc_size=256):
                 channels[i],
                 channels[i + 1],
                 kernel_size=kernel_size,
-                padding="same",
+                stride=strides[i],
+                padding="valid",
             )
         )
         layers.append(nn.LeakyReLU(0.01))
@@ -30,7 +31,7 @@ def get_block_cnn(in_dims, channels, fc_size=256):
 
     # calculate the number of features after the last convolution
     with torch.no_grad():
-        x = torch.zeros((1, *in_dims))
+        x = torch.zeros((1, 1, *in_dims))       # batch_size, channels, h, w
         for layer in layers:
             x = layer(x)
         n_features = x.shape[1]

src/dataloader.py
@@ -10,9 +10,9 @@ import cv2
 
 
 class BreastCancerDataset(Dataset):
-    def __init__(self, data_path):
-        self.data_path = data_path
-        self.data = np.genfromtxt(data_path, delimiter=",", dtype=str)
+    def __init__(self, data, img_path):
+        self.data = data
+        self.img_path = img_path
 
     def __getitem__(self, index):
         print("getdata monaa")
@@ -26,19 +26,19 @@ class BreastCancerDataset(Dataset):
         print(index)
         # read data to torch tensors
         l_cc_image = cv2.imread(
-            "/home/lan/Desktop/joskogled/data/processed_data" + l_cc,
+            self.img_path + l_cc,
             cv2.IMREAD_UNCHANGED,
         )
         l_mlo_image = cv2.imread(
-            "/home/lan/Desktop/joskogled/data/processed_data" + l_mlo,
+            self.img_path + l_mlo,
             cv2.IMREAD_UNCHANGED,
         )
         r_cc_image = cv2.imread(
-            "/home/lan/Desktop/joskogled/data/processed_data" + r_cc,
+            self.img_path + r_cc,
             cv2.IMREAD_UNCHANGED,
         )
         r_mlo_image = cv2.imread(
-            "/home/lan/Desktop/joskogled/data/processed_data" + r_mlo,
+            self.img_path + r_mlo,
             cv2.IMREAD_UNCHANGED,
         )
 
@@ -46,17 +46,28 @@ class BreastCancerDataset(Dataset):
         print(l_cc)
 
         # convert to torch tensors
+        l_cc_image = torch.from_numpy(l_cc_image.astype(np.float32))
+        l_mlo_image = torch.from_numpy(l_mlo_image.astype(np.float32))
+        r_cc_image = torch.from_numpy(r_cc_image.astype(np.float32))
+        r_mlo_image = torch.from_numpy(r_mlo_image.astype(np.float32))
+
         # view as 1 channel image
         # expand dims to add channel dimension
-        l_cc_image = torch.from_numpy(l_cc_image.astype(np.float16))
-        l_mlo_image = torch.from_numpy(l_mlo_image.astype(np.float16))
-        r_cc_image = torch.from_numpy(r_cc_image.astype(np.float16))
-        r_mlo_image = torch.from_numpy(r_mlo_image.astype(np.float16))
-
         l_cc_image = np.expand_dims(l_cc_image, axis=0)
         l_mlo_image = np.expand_dims(l_mlo_image, axis=0)
         r_cc_image = np.expand_dims(r_cc_image, axis=0)
         r_mlo_image = np.expand_dims(r_mlo_image, axis=0)
+
+
+        # change data type from str to tensor
+        years_to_cancer = int(years_to_cancer)
+
+        # for classification, map index 100 to 0, so that all possible values become (0, 1, 2, 3, 4)
+        if years_to_cancer == 100:
+            years_to_cancer = 0
+
+        # convert to tensor
+        years_to_cancer = torch.tensor(years_to_cancer)
 
         return (
             patient_id,
@@ -71,7 +82,20 @@ class BreastCancerDataset(Dataset):
         return len(self.data)
 
 
-def get_dataloader(data_path, batch_size=16, shuffle=False):
-    dataset = BreastCancerDataset(data_path)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
-    return dataloader
+def get_dataloader(data_path, img_path, batch_size=16, shuffle=False, p=0.7):
+    # read data
+    data = np.genfromtxt(data_path, delimiter=",", dtype=str)
+
+    # shuffle data in place
+    rng = np.random.default_rng(seed=3)
+    rng.shuffle(data, axis=0)
+
+    # clac dividing index and divide the dataset
+    div_index = int(p*data.shape[0])
+    dataset_train = BreastCancerDataset(data[:div_index], img_path)
+    dataset_val = BreastCancerDataset(data[div_index:], img_path)
+
+    # create dataloaders
+    dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=shuffle)
+    dataloader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=shuffle)
+    return dataloader_train, dataloader_val

src/main.py
@@ -1,8 +1,6 @@
-# import from relative path
-
-
 from dataloader import get_dataloader
 from model import Model
+from trainer import Trainer
 import torch.nn as nn
 import torch
 
@@ -15,17 +13,25 @@ sys.path.append(PATH_PREFIX)
 
 
 def main():
-    dtl = get_dataloader("/home/lan/Desktop/joskogled/data/processed_data.txt")
+    dtl_train, dtl_val = get_dataloader(
+        data_path="../data/processed_data.txt",
+        img_path="../data/processed_data",
+        batch_size=8,
+        shuffle=True,
+        p=0.7
+    )
     print("mogoce dela")
-    criterion = nn.CrossEntropyLoss()
     model = Model(
-        in_dims_cc=(2048, 1664),
-        in_dimms_mlo=(2048, 1664),
-        channels=[1, 4],
-        fc_size=2,
+        in_dims=(2048, 1576),
+        out_classes=5,              # 0, 1, 2, 3, 4
+        channels=[1, 16, 32, 32, 64, 128],
+        strides=[2, 2, 2, 1, 1],
+        fc_size=128,
     )
     print("ne res ja")
+    criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    trainer = Trainer(model, criterion, optimizer)
 
     # Train one epoch
     NUM_EPOCHS = 1
@@ -34,32 +40,38 @@ def main():
         # Train
         # model.train()
         for batch_idx, (
-            patient_id,
-            l_cc,
-            l_mlo,
-            r_cc,
-            r_mlo,
-            years_to_cancer,
-        ) in enumerate(dtl):
+                patient_id,
+                l_cc,
+                l_mlo,
+                r_cc,
+                r_mlo,
+                years_to_cancer,
+        ) in enumerate(dtl_train):
 
             print(patient_id)
             print(l_cc.shape)
-            break
-            # Forward pass
-            output = model(l_cc, l_mlo, r_cc, r_mlo)
-            loss = criterion(output, years_to_cancer)
 
-            # Backward and optimize
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            loss = trainer.train((l_cc, l_mlo, r_cc, r_mlo), years_to_cancer)
 
             if (batch_idx + 1) % 10 == 0:
                 print(
                     "Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}".format(
-                        epoch + 1, NUM_EPOCHS, batch_idx + 1, len(dtl), loss.item()
+                        epoch + 1, NUM_EPOCHS, batch_idx + 1, len(dtl_train), loss.item()
                     )
                 )
+        # after every epoch calculate val loss
+        val_loss = 0
+        batch_idx = 0
+        for batch_idx, (
+                patient_id,
+                l_cc,
+                l_mlo,
+                r_cc,
+                r_mlo,
+                years_to_cancer,
+        ) in enumerate(dtl_val):
+            val_loss += trainer.eval((l_cc, l_mlo, r_cc, r_mlo), years_to_cancer)
+        val_loss /= (batch_idx + 1)
 
 
 if __name__ == "__main__":

src/model.py
@@ -7,14 +7,14 @@ FC_SIZE = 256
 
 class Model(nn.Module):
     def __init__(
-        self, in_dims_cc, in_dimms_mlo, channels=[1, 32, 64, 128], fc_size=FC_SIZE
+        self, in_dims, out_classes, channels, strides, fc_size=FC_SIZE
     ):
         super(Model, self).__init__()
         # convolutional blocks for each image
-        self.l_cc_cnn = get_block_cnn(in_dims_cc, channels, fc_size)
-        self.l_mlo_cnn = get_block_cnn(in_dimms_mlo, channels, fc_size)
-        self.r_cc_cnn = get_block_cnn(in_dims_cc, channels, fc_size)
-        self.r_mlo_cnn = get_block_cnn(in_dimms_mlo, channels, fc_size)
+        self.l_cc_cnn = get_block_cnn(in_dims, channels, strides, fc_size)
+        self.l_mlo_cnn = get_block_cnn(in_dims, channels, strides, fc_size)
+        self.r_cc_cnn = get_block_cnn(in_dims, channels, strides, fc_size)
+        self.r_mlo_cnn = get_block_cnn(in_dims, channels, strides, fc_size)
 
         # concatenate the outputs of the convolutional blocks
         self.block_classifier = nn.Sequential(
@@ -24,7 +24,7 @@ class Model(nn.Module):
             nn.LeakyReLU(0.01),
             nn.Linear(fc_size, fc_size // 2),
             nn.LeakyReLU(0.01),
-            nn.Linear(fc_size // 2, 1),
+            nn.Linear(fc_size // 2, out_classes),
             nn.Softmax(dim=1),
         )
 