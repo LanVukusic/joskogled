cd817b1191fb9e412fefd8de8d74010be69059a9
slurmy <slurmy.slurm.slurm>
2023-03-30 12:16:21 +0200

    nova

--- src/block_classifier.py
+++ src/block_classifier.py
@@ -1,9 +1,10 @@
 import torch.nn as nn
 
-def get_block_classifier(in_size, sizes):
-    sizes = in_size + sizes
+def get_block_classifier(in_size, sizes, out_classes):
+    sizes = [in_size] + sizes
     layers = []
     for i in range(len(sizes) - 1):
         layers.append(nn.Linear(sizes[i], sizes[i+1]))
         layers.append(nn.LeakyReLU(0.01))
+    layers.append(nn.Linear(sizes[-1], out_classes))
     return nn.Sequential(*layers)
--- src/models.py
+++ src/models.py
@@ -11,9 +11,14 @@ class Model(nn.Module):
         # convolutional blocks for each image
         self.block_cnn = get_block_cnn_pool(in_dims, channels, strides)
 
+        # reduce from 4 cnn block
+        self.cat_reduce_dim = nn.Sequential(
+            nn.Linear(4 * channels[-1], channels[-1])
+            nn.LeakyReLU(0.01)
+        )
+
         # concatenate the outputs of the convolutional blocks
         self.block_classifier = get_block_classifier(channels[-1], fc_sizes, out_classes)
-        self.to_classes = nn.Linear(fc_sizes[-1], out_classes)
 
     def forward(self, l_cc, l_mlo, r_cc, r_mlo):
         l_cc = self.block_cnn(l_cc)
@@ -22,8 +27,8 @@ class Model(nn.Module):
         r_mlo = self.block_cnn(r_mlo)
 
         cat = torch.cat([l_cc, l_mlo, r_cc, r_mlo], dim=1)
-        x = self.block_classifier(cat)
-        x = self.to_classes(x)
+        x = self.cat_reduce_dim(cat)
+        x = self.block_classifier(x)
         return x
 
 class Model4(nn.Module):
