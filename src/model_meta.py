5b5c516c95c2572f81bf717fd42dde0ff6a67372
Lan Vukušič <lan.vukusic@gmail.com>
2023-04-05 21:30:52 +0200

    metrics

--- src/model_meta.py
+++ src/model_meta.py
@@ -1,5 +1,7 @@
 import torchmetrics
 import torch
+from torch.utils.tensorboard import SummaryWriter
+import random
 
 
 class ModelMetrics:
@@ -9,6 +11,7 @@ class ModelMetrics:
         single_form="{}: {:.2f} | ",
         compute_form="  -: {}: {:.2f}\n",
         batch_epoch_form="[B{:02d}  E{:02d}] | ",
+        comment="",
     ):
         self.metrics = {}
         self.device = device
@@ -18,6 +21,14 @@ class ModelMetrics:
 
         self.current_batch = None
         self.current_epoch = None
+
+        self.loss = torchmetrics.MeanMetric()
+
+        if comment == "":
+            self.writer = SummaryWriter()
+        else:
+            comment = comment.replace(" ", "_")
+            self.writer = SummaryWriter(comment=comment)
 
     def format_single(self, key, value, form=None):
         if form is None:
@@ -41,19 +52,25 @@ class ModelMetrics:
         self.current_batch = None
         self.current_epoch = None
 
-    def update(self, y_pred, y_true, show=False, batch=None, epoch=None):
+    def update(self, y_pred, y_true, loss, show=False, batch=None, epoch=None):
         self.current_batch = batch
         self.current_epoch = epoch
+
+        self.loss.update(loss)
+        self.writer.add_scalar("loss", loss, epoch)
 
         result = {}
         for name, metric in self.metrics.items():
             result[name] = metric(y_pred, y_true)
+            if epoch != None:
+                self.writer.add_scalar(name, result[name], epoch)
         if show:
             self.show(result)
         return result
 
     def compute(self, show=False):
         result = {}
+        self.loss.compute()
         for name, metric in self.metrics.items():
             result[name] = metric.compute()
         if show:
@@ -73,6 +90,9 @@ class ModelMetrics:
         out_str = self.get_epoc_batch_display()
         for metric_name, metric_value in result.items():
             out_str += self.format_single(metric_name, metric_value)
+
+        # print loss
+        out_str += self.format_single("loss", self.loss.compute())
         print(out_str)
 
     def show_compute(self, result):
@@ -83,13 +103,22 @@ class ModelMetrics:
 
         for metric_name, metric_value in result.items():
             out_str += self.format_compute(metric_name, metric_value)
+
+        # print loss
+        out_str += self.format_compute("loss", self.loss.compute())
+
         print(out_str)
+
+    # destructor
+    def __del__(self):
+        self.writer.flush()
+        self.writer.close()
 
 
 if __name__ == "__main__":
     # set device to GPU
     DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    metrics = ModelMetrics(device=DEVICE)
+    metrics = ModelMetrics(device=DEVICE, comment="moj_model_2")
     metrics.add_metric(
         "auroc",
         torchmetrics.AUROC(task="multiclass", num_classes=2, average="macro"),
@@ -109,8 +138,9 @@ if __name__ == "__main__":
             # generate dummy data float32 with 2 classes
             y_pred = torch.rand(16, 2, dtype=torch.float32)
             y_true = torch.randint(0, 2, (16,), dtype=torch.int64)
+            random_loss = random.Random()
 
-            metrics.update(y_pred, y_true, show=True, batch=j, epoch=i)
+            metrics.update(y_pred, y_true, random_loss, show=True, batch=j, epoch=i)
 
         metrics.compute(show=True)
         metrics.reset()
